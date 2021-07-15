05c15ba13466018d4c0247ff9ff19cefddd02830
KevinKentieCIMSOLUTIONS <k.kentie@cimsolutions.nl>
2021-07-15 09:33:29 +0200

    Hello world to base URL instead of /welcome

--- src/main/java/com/cimsolutions/devopsdemo/controller/WelcomeController.java
+++ src/main/java/com/cimsolutions/devopsdemo/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class WelcomeController {
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String test(){
         return "welcome";
     }
